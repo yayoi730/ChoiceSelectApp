ea9877a0ed97a25ea377c54a016342efea6b91eb
brendan <brend@DESKTOP-QUNPOG0>
2020-11-24 12:35:01 -0500

    combine, hopefully this works

---
+++ ChoiceSelectApp/js/login.js
@@ -0,0 +1,62 @@
+/*
+
+*/
+
+function handleLoginClick(e) {
+	
+	//for the choiceId check if member already exists
+	//if not, create new member with those credentials
+	//then bring user to main interface page
+	
+	//get inputs, disregard choiceId input
+	var form = document.loginForm;
+	var data = {};
+	data["username"] = form.username.value;
+	data["password"] = form.password.value;
+	data["choiceId"] = fo rm.choiceId.value;
+	
+	//convert data to JSON
+	var js = JSON.stringify(data);
+	console.log("JS:" + js);
+	var xhr = new XMLHttpRequest();
+
+	var specified = "/" + choiceId + "/" + username + "/" + password;
+	xhr.open("POST", login_url + specified, true)
+	
+	//send to API as JSON
+	xhr.send(js)
+	
+	
+	
+	//switch to interface page
+	
+}
+
+
+
+function handleAdminLogin(e) {
+	
+	//if login matches admin, bring to admin page
+	//otherwise show error message and stay on page
+	
+	//get inputs, disregard choiceId input
+	var form = document.loginForm;
+	var data = {};
+	data["username"] = form.username.value;
+	data["password"] = form.password.value;
+	
+	//convert data to JSON
+	var js = JSON.stringify(data);
+	console.log("JS:" + js);
+	var xhr = new XMLHttpRequest();
+
+	var specified = "/" + username + "/" + password;
+	xhr.open("POST", login_url + specified, true)
+	
+	//send to API as JSON
+	xhr.send(js)
+	
+	
+	//switch to admin page
+	
+}
