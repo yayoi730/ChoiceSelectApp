9fbb168c313c96063eb81518484642cb89449251
Ryan Doyle <rbdoyle@wpi.edu>
2020-12-03 12:31:31 -0500

    Fix for opinion request

--- ChoiceSelectApp/src/main/java/choice/select/app/http/OpinionRequest.java
+++ ChoiceSelectApp/src/main/java/choice/select/app/http/OpinionRequest.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 
 public class OpinionRequest {
 	String aID;
-	String approver;
+	String prover;
 	boolean approves;
 	
 	public OpinionRequest() {}
 	public OpinionRequest(String prover, boolean approves, String aID ) {
-		this.approver = prover;
+		this.prover = prover;
 		this.approves = approves;
 		this.aID = aID;
 	}
 	public String getaID() {return aID;}
 	public void setaID(String aID) {this.aID = aID;}
 	
-	public String getProver() {return approver;}
-	public void setProver(String prover) {this.approver = prover;}
+	public String getProver() {return this.prover;}
+	public void setProver(String prover) {this.prover = prover;}
 	
 	public Boolean getApproves() {return approves;}
 	public void setApproves(Boolean approves) {this.approves = approves;}
