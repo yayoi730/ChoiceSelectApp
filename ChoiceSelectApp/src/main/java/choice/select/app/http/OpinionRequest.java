44e98f45598874758a7877d1625d4ac82c026191
yayoi730 <eringcarter@gmail.com>
2020-12-02 20:04:01 -0500

    k

--- ChoiceSelectApp/src/main/java/choice/select/app/http/OpinionRequest.java
+++ ChoiceSelectApp/src/main/java/choice/select/app/http/OpinionRequest.java
@@ -1,5 +1,27 @@
 package choice.select.app.http;
 
+import java.util.ArrayList;
+
 public class OpinionRequest {
+	ArrayList<String> approvers;
+	ArrayList<String> disapprovers;
+	String aID;
+	String prover;
+	boolean approves;
+	
+	public OpinionRequest() {}
+	public OpinionRequest(String prover, boolean approves, String aID ) {
+		this.prover = prover;
+		this.approves = approves;
+		this.aID = aID;
+	}
+	public String getaID() {return aID;}
+	public void setaID(String aID) {this.aID = aID;}
+	
+	public String getProver() {return prover;}
+	public void setProver(String prover) {this.prover = prover;}
+	
+	public Boolean getApproves() {return approves;}
+	public void setApproves(Boolean approves) {this.approves = approves;}
 
 }
--- ChoiceSelectApp/src/main/java/choice/select/app/http/OpinionResponse.java
+++ ChoiceSelectApp/src/main/java/choice/select/app/http/OpinionResponse.java
@@ -1,5 +1,23 @@
 package choice.select.app.http;
 
+import java.util.ArrayList;
+
+import rando.randomness.app.demo.model.Report;
+
 public class OpinionResponse {
+	public final int httpCode;
+	public final String error;
+	
+	public OpinionResponse(ArrayList<Report> r) {
+		this.reports = r;
+		this.httpCode = 200;
+		this.error = "";
+	}
+	
+	public OpinionResponse(int code, String err) {
+		this.reports = new ArrayList<Report>();
+		this.httpCode = code;
+		this.error = err;
+	}
 
 }
